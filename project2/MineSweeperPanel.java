8e4236fa465c593d6fa57613a1e69ade6eb526fa
mastersd <mastersd@mail.gvsu.edu>
2016-02-02 11:56:57 -0500

    Updated buttons and listeners

--- project2/MineSweeperPanel.java
+++ project2/MineSweeperPanel.java
@@ -1,6 +1,9 @@
 package project2;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,18 +13,35 @@ public class MineSweeperPanel extends JPanel {
 	private JButton[][] board;
 	private JButton butQuit;
 	private Cell iCell;
+	private JSlider boardSize;
 
 	private MineSweeperGame game;  // model
 
-
 	public MineSweeperPanel() {
-
+		// create JPanels
 		JPanel bottom = new JPanel();
 		JPanel center = new JPanel();
-
-		// create game, listeners
+		
+		// create buttons/sliders/panes
+		butQuit = new JButton("Quit");
+		boardSize = new JSlider(JSlider.HORIZONTAL);
+		boardSize.setMinimum(3);
+		boardSize.setMaximum(30);
+		boardSize.setMajorTickSpacing(3);
+		boardSize.setMinorTickSpacing(1);
+		boardSize.setPaintTicks(true);
+		boardSize.setPaintLabels(true);
+		
+		// prompt user for board size using JOptionPane and slider
+		JOptionPane.showInputDialog(null, new Object[] { "Select a value: ", boardSize});
+		
+		// create button listener
 		ButtonListener listener = new ButtonListener();
 
+		// add listener for quit button
+		butQuit.addActionListener(listener);
+		
+		// instantiate new MineSweeperGame
 		game = new MineSweeperGame();
 
 		// create the board
@@ -34,18 +54,19 @@ public class MineSweeperPanel extends JPanel {
 				board[row][col].addActionListener(listener);
 				center.add(board[row][col]);
 			}
-
+		
+		// add quit button to layout
+		bottom.add(butQuit);
+	
 		displayBoard();
 
+		// 
 		bottom.setLayout(new GridLayout(3, 2));
 
 		// add all to contentPane
-		add(new JLabel("!!!!!!  Mine Sweeper  !!!!"), BorderLayout.NORTH);
 		add(center, BorderLayout.CENTER);
 		add(bottom, BorderLayout.SOUTH);
-
 	}
-
 
 	private void displayBoard() {
 
@@ -67,7 +88,6 @@ public class MineSweeperPanel extends JPanel {
 			}
 	}
 
-
 	private class ButtonListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
@@ -76,7 +96,10 @@ public class MineSweeperPanel extends JPanel {
 				for (int c = 0; c < 5; c++)
 					if (board[r][c] == e.getSource())
 						game.select(r, c);
-
+			
+			if(butQuit == e.getSource())
+				System.exit(0);
+				
 			displayBoard();
 								
 			if (game.getGameStatus() == GameStatus.Lost) {
@@ -89,13 +112,12 @@ public class MineSweeperPanel extends JPanel {
 			}
 
 			if (game.getGameStatus() == GameStatus.Won) {
-				JOptionPane.showMessageDialog(null, "You Win: all mines have been found!\n The game will reset");
+				JOptionPane.showMessageDialog(null, "You Win: all mines have been found!\n"
+						+ "The game will reset");
 				game.reset();
 				displayBoard();
 			}
-
 		}
-
 	}
-
+	
 }
